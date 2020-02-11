5dac92dfb62b5342168ea95cba9cab950c23e225
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-10 21:57:27 -0800

    card generation for major arcana added and previous syntactical issues resolved

--- tarot/Models/Deck.cs
+++ tarot/Models/Deck.cs
@@ -15,9 +15,19 @@ namespace tarot.Models
                 for(int j = 0; j < 4; j++)
                 {
                     Card card = new Card();
-                    Card.Face = (Face)i;
-                    Card.Suit = (Suit)i;
+                    card.Face = (Face)i;
+                    card.Minor = (Minor)j;
                     Cards.Add(card);
+                }
+            }
+
+            for(int k = 0; k <22; k++)
+            {
+                for (int l = 0; l < 1; j++)
+                {
+                    Card card = new Card();
+                    card.Face = (Face)k;
+                    card.Major = (Major)l;
                 }
             }
         }
