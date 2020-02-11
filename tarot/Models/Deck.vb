528f61e0c334412e6f78b0bb7d3dd3e76cb8f27c
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-10 21:14:37 -0800

    some of deck/card  creation added

--- tarot/Models/Deck.vb
+++ tarot/Models/Deck.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Deck
+    Public Deck()
+    {
+    Cards = New List<Card>();
 
+    For (int i = 0; i < 14; i++)
+        {
+            For (int j = 0; j < 4; j++)
+            {
+                Card card = New Card();
+                Card.Face = (Face)i;
+                Card.Suit = (Suit)i;
+                Cards.Add(card);
+            }
+        }
+    }
 End Class
