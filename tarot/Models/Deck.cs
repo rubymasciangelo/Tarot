274eb9ac997749a368d5d5e2f5edd27a3970b87d
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-10 21:46:03 -0800

    deck.vb changed to deck.cs wow

---
+++ tarot/Models/Deck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tarot.Models
+{
+    public class Deck
+    {
+        Public Deck()
+        {
+            Cards = new List<Card>();
+
+            for(int i = 0; i < 14; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Card card = new Card();
+                    Card.Face = (Face)i;
+                    Card.Suit = (Suit)i;
+                    Cards.Add(card);
+                }
+            }
+        }
+    }
+}
