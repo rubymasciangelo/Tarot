3f62f232fadc1344554cd505961b0467f5aaea88
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-12 15:24:54 -0800

    subject class hand created

--- tarot/Models/Subject.cs
+++ tarot/Models/Subject.cs
@@ -1,7 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace tarot.Models
 {
+    public class Subject
+    {
+        public Subject()
+        {
+            Hand = new List<Card>();
+        }
+    private List<Card> hand = new List<Card>();
+    public List<Card> Hand { get { return hand; } }
+
+    }
 }
