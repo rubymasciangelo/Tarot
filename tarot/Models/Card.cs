447fe82da64efbc818765f810cfb126cdd7a4ff8
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-11 16:58:45 -0800

    cards instantiated

--- tarot/Models/Card.cs
+++ tarot/Models/Card.cs
@@ -4,5 +4,22 @@ using System.Linq;
 
 namespace tarot.Models
 {
+    public class  Card
+    {
+        public FaceMinor FaceMinor { get; set; }
+        public Minor Minor { get; set; }
+        public FaceMajor FaceMajor { get; set; }
+        public Major Major { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", FaceMinor, Minor ^ FaceMajor, Major);
+
+            if(isReverse = "true")
+            {
+                return " in reverse";
+            }
+        }
+    }
 
 }
