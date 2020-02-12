6d21819a5c1982c4b62b460a6bc8c85505c470f1
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-12 15:40:11 -0800

    some information added to oracle

--- tarot/Models/Oracle.cs
+++ tarot/Models/Oracle.cs
@@ -6,6 +6,11 @@ namespace tarot.Models
 {
     public class Oracle
     {
+        public Deck Deck { get; set; }
 
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards)
+        }
     }
 }
