cdb3af6625d7a6fcb442ee66f3fd6004bb400976
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-12 15:44:02 -0800

    tarotoracle class added

---
+++ tarot/Models/TarotOracle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace tarot.Models
+{
+    public class TarotOracle : Oracle
+    {
+        private List<Card> hand = new List<Card>();
+        public List<Card> Hand { get { return hand; } }
+    }
+}
