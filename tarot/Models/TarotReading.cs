2d4ff354b374802d248c22d0379618f113d5a8ee
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-13 22:06:19 -0800

    some things added to tarotreading

--- tarot/Models/TarotReading.cs
+++ tarot/Models/TarotReading.cs
@@ -5,5 +5,17 @@ using System.Linq;
 namespace tarot.Models
 {
     public class TarotReading : Reading
-
+    {
+        public TarotReading Oracle { get; set; }
+        public override void Divine()
+        {
+            Oracle = new TarotOracle();
+            foreach (Subject subject in Subjects)
+            {
+                subject.Hand = new List<Card>();
+            }
+            Oracle.Deck = new Deck();
+            Oracle.Deck.Shuffle();
+        }
+    }
 }
