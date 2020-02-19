25ac02fd06778dff6a46472f35435916b89a135f
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-19 01:54:51 -0800

    subject class added to test console app

---
+++ Tarot_Test/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarot_Test
+{
+    public class Subject
+    {
+        public Subject()
+        {
+            Hand = new List<Card>();
+        }
+        private List<Card> hand = new List<Card>();
+        public List<Card> Hand { get { return hand; } }
+
+    }
+}
