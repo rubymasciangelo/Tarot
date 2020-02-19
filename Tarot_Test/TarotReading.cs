ce767c7b8475814abaf309cbacfecf08e8d12be3
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-19 01:59:56 -0800

    tarottest class added to test console app

---
+++ Tarot_Test/TarotReading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tarot_Test
+{
+    public class TarotReading : Reading
+    {
+        public TarotReading Oracle { get; set; }
+        public override void Divine()
+        {
+            Oracle = new TarotOracle();
+            foreach (Subject subject in Subjects)
+            {
+                subject.Hand = new List<Card>();
+            }
+            Oracle.Deck = new Deck();
+            Oracle.Deck.Shuffle();
+        }
+    }
+}
