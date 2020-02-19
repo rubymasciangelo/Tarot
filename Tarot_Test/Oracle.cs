e6b96ca99f08f731c9ddce87e531754778d98de5
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-19 01:30:03 -0800

    oracle class added to test environment

---
+++ Tarot_Test/Oracle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Tarot_Test
+{
+    public class Oracle
+    {
+        public Deck Deck { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards);
+        }
+    }
+}
