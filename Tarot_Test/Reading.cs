d87429e22c7b33c2fa17dd3e4336291c999eb229
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-19 01:31:45 -0800

    reading class added to test environment

---
+++ Tarot_Test/Reading.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tarot_Test
+{
+    public abstract class Reading
+    {
+        public abstract void Divine();
+    }
+}
