388761318117f9e2ccca9d6a37e55c28d72f1d1a
rubymasciangelo <rubymasciangelo@gmail.com>
2020-02-19 01:56:39 -0800

    tarotoracle class added to test console app

---
+++ Tarot_Test/TarotOracle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tarot_Test
+{
+    public class TarotOracle : Oracle
+    {
+        private List<Card> hand = new List<Card>();
+        public List<Card> Hand { get { return hand; } }
+    }
+}
